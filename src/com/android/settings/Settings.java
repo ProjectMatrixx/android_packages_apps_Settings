4233124f4781057cf1864109ff4b55479afc486c
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Settings: Allow to quickly open Running Services from meminfo view
    @neobuddy89: Updated for A14.
    
    Change-Id: I252ce85bda3daab5c5e4db097a6fd743365bc309
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -482,4 +482,6 @@ public class Settings extends SettingsActivity {
     public static class ScreenTimeoutActivity extends SettingsActivity { /* empty */ }
 
     public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
+
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
