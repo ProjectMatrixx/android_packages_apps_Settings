4cefae9e5ad958847fde1af07ed4ef8775f68163
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate MatrixxSettings into Settings
    Change-Id: Id86228181a2ae84008a177d42d1fc94317eb2dc8
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -653,4 +653,6 @@ public class Settings extends SettingsActivity {
         }
     }
     public static class SafetyCenterActivity extends SettingsActivity { }
+
+    public static class MatrixxSettingsLayoutActivity extends SettingsActivity {}
 }
