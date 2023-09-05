maldini03/android_packages_apps_Settings-2

Settings: Add toggle for Custom UI [2/2]
* TODO: Refresh systemUI

Change-Id: I541f18ff25280f7b117aeb949a14f2efc1ab5b5c
Signed-off-by: aswin7469 <aswinas@pixysos.com>

src/com/android/settings/DisplaySettings.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
+import com.android.settings.display.CustomUIPreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.LiveDisplayPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
@@ -77,6 +78,7 @@ public class DisplaySettings extends DashboardFragment {
             Context context, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new CameraGesturePreferenceController(context));
+        controllers.add(new CustomUIPreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));

src/com/android/settings/display/CustomUIPreferenceController.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
+
+public class CustomUIPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
+    public CustomUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_CUSTOM_UI_TOGGLE;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int customUIToggleValue = Settings.System.getInt(mContext.getContentResolver(),
+                CUSTOM_UI_TOGGLE, 0);
+        ((SwitchPreference) preference).setChecked(customUIToggleValue != 0);
+    }
+
+    @Override
+    public boolean isAvailable() {
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean customUIToggleValue = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(), CUSTOM_UI_TOGGLE, customUIToggleValue ? 1 : 0);
+        return true;
+    }
+}

ProjectMatrixx/android_packages_apps_Settings

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/settings/DisplaySettings.java
@@ -35,6 +35,8 @@ import com.android.settingslib.search.SearchIndexable;
 
 import lineageos.hardware.LineageHardwareManager;
 
+import com.android.settings.preferences.CustomUIPreferenceController;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -86,6 +88,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new CustomUIPreferenceController(context));
         return controllers;
     }
 

src/com/android/settings/preferences/CustomUIPreferenceController.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.preferences;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.internal.util.crdroid.Utils;
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
+
+public class CustomUIPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_CUSTOM_UI_TOGGLE = "custom_ui_toggle";
+
+    public CustomUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_CUSTOM_UI_TOGGLE;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int customUIToggleValue = Settings.System.getInt(mContext.getContentResolver(),
+                CUSTOM_UI_TOGGLE, 0);
+        ((SwitchPreference) preference).setChecked(customUIToggleValue != 0);
+    }
+
+    @Override
+    public boolean isAvailable() {
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean customUIToggleValue = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(), CUSTOM_UI_TOGGLE, customUIToggleValue ? 1 : 0);
+        Utils.showSettingsRestartDialog(mContext);
+        return true;
+    }
+}