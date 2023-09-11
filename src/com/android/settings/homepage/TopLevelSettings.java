theangelofdead01/android_packages_apps_Settings-1

[SQUASH]: Settings Homepage Redesign
Co-authored-by: beingmishra <rahul731999@gmail.com>
Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
Signed-off-by: str143 <alexfinhart@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -64,6 +64,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private int mPaddingHorizontal;
     private boolean mScrollNeeded = true;
     private boolean mFirstStarted = true;
+    private boolean gAppsExists;
 
     public TopLevelSettings() {
         final Bundle args = new Bundle();
@@ -191,7 +192,48 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             if (icon != null) {
                 icon.setTint(tintColor);
             }
+            onSetPrefCard();
         });
+    }
+
+    private void onSetPrefCard() {
+	final PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+
+            final Preference preference = screen.getPreference(i);
+ 	    String key = preference.getKey();
+            	
+	    if (key.equals("usercard_space")){
+                preference.setLayoutResource(R.layout.usercard_space);
+            } else if (key.equals("top_level_network")
+            	|| key.equals("top_level_rice")
+            	|| key.equals("top_level_apps")
+            	|| key.equals("top_level_accessibility")
+            	|| key.equals("top_level_emergency")
+            	|| key.equals("top_level_system")){
+                preference.setLayoutResource(R.layout.top_level_preference_top);
+            } else if (key.equals("top_level_battery")
+            	|| key.equals("top_level_display")
+            	|| key.equals("top_level_security")
+            	|| key.equals("top_level_privacy")
+            	|| key.equals("top_level_storage")
+            	|| key.equals("top_level_notifications")){
+                preference.setLayoutResource(R.layout.top_level_preference_middle);
+            } else if (key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity")
+            	|| key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.home.TopLevelSettingsActivity")
+            	|| key.equals("top_level_wellbeing")){
+                preference.setLayoutResource(R.layout.top_level_preference_wellbeing);
+            } else if (key.equals("dashboard_tile_pref_com.google.android.gms.app.settings.GoogleSettingsIALink")
+            	|| key.equals("top_level_google")){
+                preference.setLayoutResource(R.layout.top_level_preference_google);
+                gAppsExists = true;
+            } else if (key.equals("top_level_accounts") && gAppsExists){
+                preference.setLayoutResource(R.layout.top_level_preference_middle);
+            } else {
+                preference.setLayoutResource(R.layout.top_level_preference_bottom);
+            }
+       }
     }
 
     @Override

ProjectMatrixx/android_packages_apps_Settings

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -66,6 +66,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private boolean mScrollNeeded = true;
     private boolean mFirstStarted = true;
     private ActivityEmbeddingController mActivityEmbeddingController;
+    private boolean gAppsExists;
 
     public TopLevelSettings() {
         final Bundle args = new Bundle();
@@ -203,7 +204,48 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             if (icon != null) {
                 icon.setTint(tintColor);
             }
+            onSetPrefCard();
         });
+    }
+
+    private void onSetPrefCard() {
+	final PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+
+            final Preference preference = screen.getPreference(i);
+ 	    String key = preference.getKey();
+            	
+	    if (key.equals("usercard_space")){
+                preference.setLayoutResource(R.layout.usercard_space);
+            } else if (key.equals("top_level_network")
+            	|| key.equals("top_level_rising")
+            	|| key.equals("top_level_apps")
+            	|| key.equals("top_level_accessibility")
+            	|| key.equals("top_level_emergency")
+            	|| key.equals("top_level_system")){
+                preference.setLayoutResource(R.layout.top_level_preference_top);
+            } else if (key.equals("top_level_battery")
+            	|| key.equals("top_level_display")
+            	|| key.equals("top_level_security")
+            	|| key.equals("top_level_privacy")
+            	|| key.equals("top_level_storage")
+            	|| key.equals("top_level_notifications")){
+                preference.setLayoutResource(R.layout.top_level_preference_middle);
+            } else if (key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity")
+            	|| key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.home.TopLevelSettingsActivity")
+            	|| key.equals("top_level_wellbeing")){
+                preference.setLayoutResource(R.layout.top_level_preference_wellbeing);
+            } else if (key.equals("dashboard_tile_pref_com.google.android.gms.app.settings.GoogleSettingsIALink")
+            	|| key.equals("top_level_google")){
+                preference.setLayoutResource(R.layout.top_level_preference_google);
+                gAppsExists = true;
+            } else if (key.equals("top_level_accounts") && gAppsExists){
+                preference.setLayoutResource(R.layout.top_level_preference_middle);
+            } else {
+                preference.setLayoutResource(R.layout.top_level_preference_bottom);
+            }
+       }
     }
 
     @Override