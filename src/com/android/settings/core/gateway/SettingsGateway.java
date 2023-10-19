04adac945eb333b457199ac8086afe282aa363e8
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-19 10:11:39 +0800

    Settings: Import Setting's homeapge & About phone ui from Rising
    - Squashed commits:
    
    Commit - 1:
    Settings: Implement homepage user interface
    * designed by: notayan99
    cc: hydrangea07
    
    Commit - 2:
    settings: Add preference utils for settings provider
    
    Commit - 3:
    utils: Add and improve preference utils
    * resolve memory leaks
    * add functionality to store settings
    * add seekbar preference utility
    
    Commit - 4:
    settings: Implement about device info user interface
    * designed by: notayan99
    cc: hydrangea07
    
    Commit - 5:
    settings: Settings UI revamp/overhaul
    
    Commit - 6:
    settings: Cleanup top level and widget preference initialization
    
    Commit - 7:
    [RisingUI]: Cleanup widgets init
    
    Co-authored-by: Ayan <79016495+notayan99@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    
    Commit - 8
    Settings: Add missing widget frame for solo preference layout
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Commit - 9
    [RisingUI] User interface cleanup
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -437,5 +437,7 @@ public class SettingsGateway {
             Settings.crDroidSettingsLayoutActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.FirmwareVersionActivity.class.getName(),
+            Settings.WallpaperSettingsActivity.class.getName(),
     };
 }
