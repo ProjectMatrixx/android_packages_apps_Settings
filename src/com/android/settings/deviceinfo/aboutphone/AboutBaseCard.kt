5431c3a93abdc78a3be5c1c1b5ae944bb1861f07
IacobIonut019 <paulionut2003@gmail.com>
2023-11-14 11:37:44 +0530

    Settings: Partial import DotOS About Phone design
    * Import iconic info cards (rom, device name, storage) only
    * Adapt to A13 QPR3
    * Use Android's official version 13 logo
    * Use colorAccent as logo background shape color so that we can enjoy monet
    
    Change-Id: Id96a691b84a297415cce99ec239813253f28e1a7
    Co-authored-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>

---
+++ src/com/android/settings/deviceinfo/aboutphone/AboutBaseCard.kt
@@ -0,0 +1,94 @@
+package com.android.settings.deviceinfo.aboutphone
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.widget.RelativeLayout
+
+import com.google.android.material.card.MaterialCardView
+
+import com.android.settings.R;
+
+import com.android.settingslib.Utils
+@SuppressLint("ClickableViewAccessibility")
+open class AboutBaseCard : MaterialCardView {
+    protected var layout: RelativeLayout
+    protected var defaultPadding = 38
+    var defaultRadius = 0
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        defaultRadius = resources.getDimensionPixelSize(R.dimen.contextual_card_corner_radius)
+        layoutParams = LayoutParams(resources.getDimensionPixelSize(R.dimen.storage_card_min_width), resources.getDimensionPixelSize(R.dimen.storage_card_min_height))
+        layout = RelativeLayout(context)
+        layout.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT)
+        layout.setPadding(defaultPadding, (defaultPadding * 1.5).toInt(), defaultPadding, (defaultPadding * 1.5).toInt())
+        layout.setBackgroundColor(resources.getColor(R.color.contextual_card_background, context.theme))
+        addView(layout)
+        radius = defaultRadius.toFloat()
+        setCardBackgroundColor(resources.getColor(R.color.contextual_card_background, context.theme))
+        cardElevation = 0f
+        strokeColor = resources.getColor(R.color.contextual_card_stroke_color, context.theme)
+        strokeWidth = resources.getDimensionPixelSize(R.dimen.contextual_card_stroke_width)
+        setTouchListener(layout)
+    }
+
+    fun setTouchListener(view: View) {
+        view.isClickable = true
+        view.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val scaleDownX = ObjectAnimator.ofFloat(
+                            this,
+                            "scaleX", 0.9f
+                    )
+                    val scaleDownY = ObjectAnimator.ofFloat(
+                            this,
+                            "scaleY", 0.9f
+                    )
+                    scaleDownX.duration = 200
+                    scaleDownY.duration = 200
+                    val scaleDown = AnimatorSet()
+                    scaleDown.play(scaleDownX).with(scaleDownY)
+                    scaleDown.start()
+                }
+                MotionEvent.ACTION_UP -> {
+                    val scaleDownX2 = ObjectAnimator.ofFloat(
+                            this, "scaleX", 1f
+                    )
+                    val scaleDownY2 = ObjectAnimator.ofFloat(
+                            this, "scaleY", 1f
+                    )
+                    scaleDownX2.duration = 200
+                    scaleDownY2.duration = 200
+                    val scaleDown2 = AnimatorSet()
+                    scaleDown2.play(scaleDownX2).with(scaleDownY2)
+                    scaleDown2.start()
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    val scaleDownX2 = ObjectAnimator.ofFloat(
+                            this, "scaleX", 1f
+                    )
+                    val scaleDownY2 = ObjectAnimator.ofFloat(
+                            this, "scaleY", 1f
+                    )
+                    scaleDownX2.duration = 200
+                    scaleDownY2.duration = 200
+                    val scaleDown2 = AnimatorSet()
+                    scaleDown2.play(scaleDownX2).with(scaleDownY2)
+                    scaleDown2.start()
+                }
+            }
+            false
+        }
+    }
+}
--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -16,11 +16,13 @@
 
 package com.android.settings.deviceinfo.aboutphone;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.UserInfo;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.UserManager;
 import android.util.Log;
@@ -51,6 +53,7 @@ import com.android.settings.widget.EntityHeaderController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
+import com.android.settingslib.widget.ActionBarShadowController;
 import com.android.settingslib.widget.LayoutPreference;
 
 import java.util.ArrayList;
@@ -71,7 +74,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
 
     private static final String LOG_TAG = "MyDeviceInfoFragment";
     private static final String KEY_EID_INFO = "eid_info";
-    private static final String KEY_MY_DEVICE_INFO_HEADER = "my_device_info_header";
 
     private final BroadcastReceiver mSimStateReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
@@ -107,7 +109,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
     @Override
     public void onStart() {
         super.onStart();
-        initHeader();
+        initActionbar();
     }
 
     @Override
@@ -211,38 +213,12 @@ public class MyDeviceInfoFragment extends DashboardFragment
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void initHeader() {
-        // TODO: Migrate into its own controller.
-        final LayoutPreference headerPreference =
-                getPreferenceScreen().findPreference(KEY_MY_DEVICE_INFO_HEADER);
-        final boolean shouldDisplayHeader = getContext().getResources().getBoolean(
-                R.bool.config_show_device_header_in_device_info);
-        headerPreference.setVisible(shouldDisplayHeader);
-        if (!shouldDisplayHeader) {
+    private void initActionbar() {
+        final ActionBar actionBar = getActivity().getActionBar();
+        if (actionBar == null) {
             return;
         }
-        final View headerView = headerPreference.findViewById(R.id.entity_header);
-        final Activity context = getActivity();
-        final Bundle bundle = getArguments();
-        final EntityHeaderController controller = EntityHeaderController
-                .newInstance(context, this, headerView)
-                .setRecyclerView(getListView(), getSettingsLifecycle())
-                .setButtonActions(EntityHeaderController.ActionType.ACTION_NONE,
-                        EntityHeaderController.ActionType.ACTION_NONE);
-
-        // TODO: There may be an avatar setting action we can use here.
-        final int iconId = bundle.getInt("icon_id", 0);
-        if (iconId == 0) {
-            final UserManager userManager = (UserManager) getActivity().getSystemService(
-                    Context.USER_SERVICE);
-            final UserInfo info = Utils.getExistingUser(userManager,
-                    android.os.Process.myUserHandle());
-            controller.setLabel(info.name);
-            controller.setIcon(
-                    com.android.settingslib.Utils.getUserIcon(getActivity(), userManager, info));
-        }
-
-        controller.done(context, true /* rebindActions */);
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
