4748b928af1f34c910bd640ec7ba46e307389507
jhenrique09 <jhenrique09.mcz@hotmail.com>
2025-01-24 09:13:20 +0100

    Settings: Set the default account provider to Google

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -1,6 +1,7 @@
 package com.android.settings.accounts;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 
 import com.android.settings.R;
@@ -9,11 +10,11 @@ import com.android.settings.overlay.FeatureFactory;
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
-        return FeatureFactory.getAppContext().getString(R.string.account_type);
+        return "com.google";
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return AccountManager.get(context).getAccountsByType(getAccountType());
+        return AccountManager.get(context).getAccountsByType("com.google");
     }
 }
