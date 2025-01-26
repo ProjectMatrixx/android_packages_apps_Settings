b9566fb03639e197d26550d38b68db5e1d4f1e32
darkphnx <dark.phnx12@gmail.com>
2025-01-26 12:15:26 +0100

    Settings:Fix card layout for vanilla build * Vanilla Build + Gapps (if gapps doesn't have backup and restore preference in settings page breaks the google card layout as its set to middle by default) * add a card layout for bottom as well and set it correctly based on prefernce present
    ref for key: https://github.com/TenX-OS/packages_apps_Settings/commit/0f3a3bca65186f47a1e565b9bee5e712d87dccfa#diff-7903215e8be655c75fedb3d86a1c7ce7d22ed40980e9307057a029959d9b1235R633
    
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -265,8 +265,12 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_wellbeing")
             	|| key.equals("top_level_notifications")){
                 preference.setLayoutResource(R.layout.matrixx_dashboard_preference_middle);
-            } else if ("top_level_google".equals(key)){
-                preference.setLayoutResource(R.layout.matrixx_dashboard_preference_google);
+            } else if (key.equals("top_level_google")){
+                if (gAppsExists && screen.findPreference("dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity") != null) {
+                    preference.setLayoutResource(R.layout.matrixx_dashboard_preference_google_v2);
+                }  else {
+                    preference.setLayoutResource(R.layout.matrixx_dashboard_preference_google);
+                }
             } else if (key.equals("top_level_accounts") && gAppsExists){
                 preference.setLayoutResource(R.layout.matrixx_dashboard_preference_middle);
             } else if (key.equals("top_level_crdroid")) {
