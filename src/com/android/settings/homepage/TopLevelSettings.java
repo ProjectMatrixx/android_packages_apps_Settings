342554ffcb4e4ae10d080d9c758a4b451ce9c22a
Arman-ATI <altafiarman5@gmail.com>
2025-03-10 19:02:59 +0000

    Settings: Add a toggle for Settings UI Styles [2/2]
    * Drkphnx: Use google default A14 layout for simple layout and also add new expressive settings page option
    
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.homepage;
 
+import static android.provider.Settings.Global.getString;
 import static com.android.settings.search.actionbar.SearchMenuController.NEED_SEARCH_ICON_IN_ACTION_BAR;
 import static com.android.settingslib.search.SearchIndexable.MOBILE;
 
@@ -30,6 +31,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.os.UserHandle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -43,6 +45,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.window.embedding.ActivityEmbeddingController;
 
 import com.android.settings.R;
+import android.provider.Settings;
 import com.android.settings.Utils;
 import com.android.settings.activityembedding.ActivityEmbeddingRulesController;
 import com.android.settings.activityembedding.ActivityEmbeddingUtils;
@@ -71,6 +74,8 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private static final String SAVED_HIGHLIGHT_MIXIN = "highlight_mixin";
     private static final String PREF_KEY_SUPPORT = "top_level_support";
 
+    private int mDashBoardStyle;
+
     private boolean mIsEmbeddingActivityEnabled;
     private TopLevelHighlightMixin mHighlightMixin;
     private int mPaddingHorizontal;
@@ -94,7 +99,16 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     @Override
     protected int getPreferenceScreenResId() {
-        return getPreferenceLayoutResId(getContext());
+        switch (mDashBoardStyle) {
+           case 0:
+               return R.xml.top_level_settings_v2;
+           case 1:
+               return R.xml.top_level_settings;
+           case 2:
+                return R.xml.top_level_settings_expressive;
+           default:
+               return R.xml.top_level_settings_v2;
+        }
     }
 
     @Override
@@ -112,6 +126,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         super.onAttach(context);
         HighlightableMenu.fromXml(context, getPreferenceScreenResId());
         use(SupportPreferenceController.class).setActivity(getActivity());
+        setDashboardStyle(context);
     }
 
     @Override
@@ -434,4 +449,9 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                     return false;
                 }
             };
+
+            private void setDashboardStyle(Context context) {
+                    mDashBoardStyle = Settings.System.getIntForUser(context.getContentResolver(),
+                    com.crdroid.settings.fragments.UserInterface.SETTINGS_DASHBOARD_STYLE, 0, UserHandle.USER_CURRENT);
+    }
 }
