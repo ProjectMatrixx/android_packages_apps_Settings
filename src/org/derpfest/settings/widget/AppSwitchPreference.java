0481cdb1b3494cf94acbbb7dd10e167722697fa1
cjh1249131356 <cjh1249131356@gmail.com>
2025-06-05 16:56:19 +0200

    Settings: Adapt for Android U QPR2
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/org/derpfest/settings/widget/AppSwitchPreference.java
+++ src/org/derpfest/settings/widget/AppSwitchPreference.java
@@ -8,32 +8,30 @@ package org.derpfest.settings.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import com.android.settings.R;
-
-import org.derpfest.custom.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 /**
  * The SwitchPreference for the pages need to show apps icon.
 */
-public class AppSwitchPreference extends SwitchPreference {
+public class AppSwitchPreference extends SwitchPreferenceCompat {
 
     public AppSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        setLayoutResource(R.layout.preference_app);
+        setLayoutResource(com.android.settingslib.widget.preference.app.R.layout.preference_app);
     }
 
     public AppSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setLayoutResource(R.layout.preference_app);
+        setLayoutResource(com.android.settingslib.widget.preference.app.R.layout.preference_app);
     }
 
     public AppSwitchPreference(Context context) {
         super(context);
-        setLayoutResource(R.layout.preference_app);
+        setLayoutResource(com.android.settingslib.widget.preference.app.R.layout.preference_app);
     }
 
     public AppSwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLayoutResource(R.layout.preference_app);
+        setLayoutResource(com.android.settingslib.widget.preference.app.R.layout.preference_app);
     }
 }
