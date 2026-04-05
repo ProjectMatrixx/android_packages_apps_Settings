119c58cc5f371ad58682bb13ba83da6ab648401a
Mrick343 <mrinmoy8429de@gmail.com>
2026-04-05 02:51:14 +0000

    fixup! Settings: Show matrixx version in About menu

--- src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
@@ -102,7 +102,7 @@ class LineageVersionDetailPreference :
         const val ACTIVITY_TRIGGER_COUNT = 3
         const val DELAY_TIMER_MILLIS = 500L
 
-        const val LINEAGE_VERSION_PROPERTY: String = "ro.matrixx.build.version"
+        const val LINEAGE_VERSION_PROPERTY: String = "ro.matrixx.display.version"
 
         const val PLATLOGO_PACKAGE_NAME: String = "org.lineageos.lineageparts"
         const val PLATLOGO_ACTIVITY_CLASS: String = PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity"
