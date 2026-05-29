AAMIRR-ALI/packages_apps_settings_matrixx

Settings: Integrate MatrixxLab into Settings

src/com/android/settings/Settings.java
@@ -653,4 +653,16 @@ public class Settings extends SettingsActivity {
         }
     }
     public static class SafetyCenterActivity extends SettingsActivity { }
+    
+    public static class MatrixxLabActivity extends SettingsActivity {}
+    
+    public static class PersonalizationsLockscreenActivity extends SettingsActivity {}
+    public static class PersonalizationsToolboxActivity extends SettingsActivity {}
+    public static class PersonalizationsThemesActivity extends SettingsActivity {}
+    public static class PersonalizationsClockFacesActivity extends SettingsActivity {}
+    public static class PersonalizationsSpoofActivity extends SettingsActivity {}
+    public static class PersonalizationsBottomActivity extends SettingsActivity {}
+    public static class PersonalizationsStatusbarActivity extends SettingsActivity {}
+    public static class PersonalizationsButtonActivity extends SettingsActivity {}
+    public static class PersonalizationsAboutActivity extends SettingsActivity {}
 }

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.core.gateway;
 
+import com.matrixx.settings.MatrixxLabSettings;
 import com.android.settings.DisplaySettings;
 import com.android.settings.IccLockSettings;
 import com.android.settings.MainClear;
@@ -220,6 +221,15 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.details.WifiNetworkDetailsFragment;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
+import com.matrixx.settings.QSPanelSettings;
+import com.matrixx.settings.About;
+import com.matrixx.settings.Sound;
+import com.matrixx.settings.Notifications;
+import com.matrixx.settings.UserInterface;
+import com.matrixx.settings.LockScreen;
+import com.matrixx.settings.Misc;
+import com.matrixx.settings.Buttons;
+import com.matrixx.settings.Statusbar;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 public class SettingsGateway {
@@ -432,6 +442,17 @@ public class SettingsGateway {
             SupervisionDashboardFragment.class.getName(),
             SummarizationPreferenceFragment.class.getName(),
             SafetyCenterFragment.class.getName(),
+            MatrixxLabSettings.class.getName(),
+            UserInterface.class.getName(),
+            Notifications.class.getName(),
+            LockScreen.class.getName(),
+            Misc.class.getName(),
+            Buttons.class.getName(),
+            Statusbar.class.getName(),
+            Sound.class.getName(),
+            QSPanelSettings.class.getName(),
+            About.class.getName()
+            
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -480,5 +501,15 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.ShadeSettingsActivity.class.getName(),
+            Settings.MatrixxLabActivity.class.getName(),
+            Settings.PersonalizationsLockscreenActivity.class.getName(),
+            Settings.PersonalizationsToolboxActivity.class.getName(),
+            Settings.PersonalizationsThemesActivity.class.getName(),
+            Settings.PersonalizationsClockFacesActivity.class.getName(),
+            Settings.PersonalizationsSpoofActivity.class.getName(),
+            Settings.PersonalizationsBottomActivity.class.getName(),
+            Settings.PersonalizationsStatusbarActivity.class.getName(),
+            Settings.PersonalizationsButtonActivity.class.getName(),
+            Settings.PersonalizationsAboutActivity.class.getName()
     };
 }

ProjectMatrixx/android_packages_apps_Settings

Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

src/com/android/settings/Settings.java
@@ -663,4 +663,16 @@ public class Settings extends SettingsActivity {
 
     // SleepMode
     public static class SleepModeActivity extends SettingsActivity {}
+
+    public static class MatrixxLabActivity extends SettingsActivity {}
+
+    public static class MatrixxUserInterfaceActivity extends SettingsActivity {}
+    public static class MatrixxQuickSettingsActivity extends SettingsActivity {}
+    public static class MatrixxStatusBarActivity extends SettingsActivity {}
+    public static class MatrixxLockscreenActivity extends SettingsActivity {}
+    public static class MatrixxMiscellaneousActivity extends SettingsActivity {}
+    public static class MatrixxSoundActivity extends SettingsActivity {}
+    public static class MatrixxNotificationsActivity extends SettingsActivity {}
+    public static class MatrixxButtonsActivity extends SettingsActivity {}
+    public static class MatrixxAboutActivity extends SettingsActivity {}
 }

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.core.gateway;
 
+import com.matrixx.settings.MatrixxLabSettings;
 import com.android.settings.DisplaySettings;
 import com.android.settings.IccLockSettings;
 import com.android.settings.MainClear;
@@ -224,6 +225,15 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.details.WifiNetworkDetailsFragment;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
+import com.matrixx.settings.fragments.quicksettings.QuickSettings;
+import com.matrixx.settings.fragments.about.About;
+import com.matrixx.settings.fragments.sound.Sound;
+import com.matrixx.settings.fragments.notifications.Notifications;
+import com.matrixx.settings.fragments.ui.UserInterface;
+import com.matrixx.settings.fragments.lockscreen.LockScreen;
+import com.matrixx.settings.fragments.misc.Miscellaneous;
+import com.matrixx.settings.fragments.buttons.Buttons;
+import com.matrixx.settings.fragments.statusbar.StatusBar;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 
@@ -444,6 +454,16 @@ public class SettingsGateway {
             SmartPixels.class.getName(),
             PrivateDnsSettings.class.getName(),
             SleepMode.class.getName(),
+            MatrixxLabSettings.class.getName(),
+            UserInterface.class.getName(),
+            Notifications.class.getName(),
+            LockScreen.class.getName(),
+            Miscellaneous.class.getName(),
+            Buttons.class.getName(),
+            StatusBar.class.getName(),
+            Sound.class.getName(),
+            QuickSettings.class.getName(),
+            About.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -495,6 +515,16 @@ public class SettingsGateway {
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
             Settings.PrivateDnsSettingsActivity.class.getName(),
-            Settings.SleepModeActivity.class.getName()
+            Settings.SleepModeActivity.class.getName(),
+            Settings.MatrixxLabActivity.class.getName(),
+            Settings.MatrixxUserInterfaceActivity.class.getName(),
+            Settings.MatrixxQuickSettingsActivity.class.getName(),
+            Settings.MatrixxStatusBarActivity.class.getName(),
+            Settings.MatrixxLockscreenActivity.class.getName(),
+            Settings.MatrixxMiscellaneousActivity.class.getName(),
+            Settings.MatrixxSoundActivity.class.getName(),
+            Settings.MatrixxNotificationsActivity.class.getName(),
+            Settings.MatrixxButtonsActivity.class.getName(),
+            Settings.MatrixxAboutActivity.class.getName(),
     };
 }